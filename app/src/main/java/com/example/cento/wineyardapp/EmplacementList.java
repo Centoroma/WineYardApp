3439d78fce7df0637893593c7bb815b1158ad015
Cento <luca.centofanti@students.hevs.ch>
2017-04-04 16:06:21 +0200

    Commit 4avril2017

--- app/src/main/java/com/example/cento/wineyardapp/EmplacementList.java
+++ app/src/main/java/com/example/cento/wineyardapp/EmplacementList.java
@@ -2,6 +2,10 @@ package com.example.cento.wineyardapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import static android.R.attr.id;
 
 public class EmplacementList extends AppCompatActivity {
 
@@ -9,6 +13,23 @@ public class EmplacementList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emplacement_list);
-        /*test*/
+
+
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.activity_emplacement_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.delete:
+
+                return true ;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
