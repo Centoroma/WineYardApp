4fe23e9165b3ed75d49deb283d238c737aacf686
Cento <luca.centofanti@students.hevs.ch>
2017-04-06 15:11:58 +0200

    Commit 4avril2017

--- app/src/main/java/com/example/cento/wineyardapp/EmplacementDetails.java
+++ app/src/main/java/com/example/cento/wineyardapp/EmplacementDetails.java
@@ -14,17 +14,10 @@ public class EmplacementDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emplacement_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
     /*test*/
