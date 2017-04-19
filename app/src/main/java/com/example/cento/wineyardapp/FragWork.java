1dd86eb87356d9678eba08af955e7bac576fc315
Cento <luca.centofanti@students.hevs.ch>
2017-04-19 18:32:47 +0200

    Commit 4avril2017

--- app/src/main/java/com/example/cento/wineyardapp/FragWork.java
+++ app/src/main/java/com/example/cento/wineyardapp/FragWork.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.bof.navigationdrawerwineyard.R;
+
+import static android.R.attr.description;
 
 /**
  * Created by flavien on 19/04/17.
@@ -18,6 +22,23 @@ public class FragWork extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_work, container, false);
+        View view = inflater.inflate(R.layout.frag_work,container, false);
+
+        String[] action ={"ABCD",
+                "DEFG",
+                "IJHG",
+                "CULER"};
+
+        ListView listView = (ListView)view.findViewById(R.id.listofwork);
+
+        ArrayAdapter<String> listviewadapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.doublelist,
+                R.id.action,
+                action
+        );
+
+        listView.setAdapter(listviewadapter);
+        return view;
     }
 }
